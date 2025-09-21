878101508019004c46b491b7690ead162f52951c
Merlin <83283194+merlinymy@users.noreply.github.com>
2025-09-20 23:20:06 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/apple-weather-app-replica/',
   },
   plugins: [
     new HtmlWebpackPlugin({
